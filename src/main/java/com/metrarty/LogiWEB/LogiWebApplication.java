f5a3a667ec01d2b020e828aad927a426fbc16652
Artem Oshkoder <metrarty@me.com>
2021-02-06 17:24:21 +0300

    Initial commit

---
+++ src/main/java/com/metrarty/LogiWEB/LogiWebApplication.java
@@ -0,0 +1,13 @@
+package com.metrarty.LogiWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiWebApplication.class, args);
+	}
+
+}
