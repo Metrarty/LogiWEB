ed98ab5194b044be8fe5590d53d25e69c20d21bc
Artem Oshkoder <metrarty@me.com>
2021-02-25 14:03:26 +0300

    t2: Add logs and java docs to Distance controller

--- src/main/java/com/metrarty/LogiWEB/boundary/DistanceController.java
+++ src/main/java/com/metrarty/LogiWEB/boundary/DistanceController.java
@@ -10,30 +10,53 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Distance controller
+ */
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Log4j2
 public class DistanceController {
     private final DistanceService distanceService;
 
+    /**
+     * Creates distance between two cities.
+     * @param distanceDto distance DTO.
+     * @return created distance
+     */
     @PostMapping("/distance/create/")
     public Distance createDistance(DistanceDto distanceDto) {
         log.info("DistanceController.createDistance was called with {}", distanceDto);
         return distanceService.createDistance(distanceDto);
     }
 
+    /**
+     * Receives all distances DTO.
+     * @return list of all distances DTO
+     */
     @GetMapping("/distance/all/")
     public List<DistanceDto> all() {
         log.info("DistanceController.all was called");
         return distanceService.findAllDistances();
     }
 
+    /**
+     * Edits exist distance DTO, selected by ID.
+     * @param distanceDto distance DTO
+     * @param id ID of distance that should be edited
+     * @return edited distance DTO
+     */
     @PutMapping("/distance/editbyid/{id}/")
     public DistanceDto editDistance(@RequestBody DistanceDto distanceDto, @PathVariable Long id) {
         log.info("DistanceController.editDistance was called with {} {}", distanceDto, id);
         return distanceService.editDistance(distanceDto, id);
     }
 
+    /**
+     * Deletes exist distance, selected by ID.
+     * @param id distance ID
+     * @return message with ID of deleted distance
+     */
     @DeleteMapping("/distance/deletebyid/{id}/")
     public String deleteDistanceById(@PathVariable Long id) {
         log.info("DistanceController.deleteDistanceById was called with {}", id);
