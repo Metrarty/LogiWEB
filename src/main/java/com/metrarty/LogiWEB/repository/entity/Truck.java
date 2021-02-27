169770cf0f526c2a764e18a1ddaca03ffb9aa4d3
Artem Oshkoder <metrarty@me.com>
2021-02-27 15:15:05 +0300

    t7 Add truck entity

--- src/main/java/com/metrarty/LogiWEB/repository/entity/Truck.java
+++ src/main/java/com/metrarty/LogiWEB/repository/entity/Truck.java
@@ -1,2 +1,19 @@
-package com.metrarty.LogiWEB.repository.entity;public class Truck {
+package com.metrarty.LogiWEB.repository.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Truck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private Long capacity;
+    @Column
+    private City location;
+    @Column
+    private Long distancePerDay;
 }
