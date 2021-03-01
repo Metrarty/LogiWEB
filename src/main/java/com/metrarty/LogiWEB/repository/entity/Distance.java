9c34c0a8e76e025b9281bc9db5eb566c4e589891
Artem Oshkoder <metrarty@me.com>
2021-03-01 21:16:55 +0300

    t2: update SQL

--- src/main/java/com/metrarty/LogiWEB/repository/entity/Distance.java
+++ src/main/java/com/metrarty/LogiWEB/repository/entity/Distance.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 public class Distance {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
     private City city1;
