f466339e0cf914551ad0dd6115c47d1b79e694bd
Artem Oshkoder <metrarty@me.com>
2021-03-22 16:44:11 +0300

    t13: Add tests

--- src/test/java/com/metrarty/LogiWEB/service/mapper/OrderMapperTest.java
+++ src/test/java/com/metrarty/LogiWEB/service/mapper/OrderMapperTest.java
@@ -1,19 +1,70 @@
 package com.metrarty.LogiWEB.service.mapper;
 
+import com.metrarty.LogiWEB.boundary.model.CargoDto;
+import com.metrarty.LogiWEB.boundary.model.OrderDto;
+import com.metrarty.LogiWEB.repository.entity.Cargo;
+import com.metrarty.LogiWEB.repository.entity.City;
+import com.metrarty.LogiWEB.repository.entity.Order;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrderMapperTest {
 
     private static final Instant NOW = Instant.now();
+
+    @InjectMocks
     private OrderMapper orderMapper;
-    private CityMapper cityMapper;
+
+
     private OrderMapper orderMapperSpy;
+
+    @Mock
+    private CargoMapper cargoMapper;
+
+    @Mock
+    private CityMapper cityMapper;
+
+    @Before
+    public void init() {
+        orderMapperSpy = Mockito.spy(orderMapper);
+    }
+
+    @Test
+    public void testToDto() {
+        //prepare
+
+        Cargo cargo = new Cargo();
+        City destination = new City();
+        LocalDate date = LocalDate.now();
+
+        Order order = new Order();
+        order.setId(1L);
+        order.setCargo(cargo);
+        order.setDestination(destination);
+       // order.setDeliveryDate(date);
+
+        OrderDto expected = new OrderDto();
+        expected.setId(order.getId());
+        expected.setCargo(cargoMapper.toDto(order.getCargo()));
+        expected.setDestination(cityMapper.toDto(order.getDestination()));
+       // order.setDeliveryDate(order.getDeliveryDate());
+
+        //run
+        OrderDto actual = orderMapper.toDto(order);
+
+        //test
+        Assert.assertEquals("Must be equals", expected, actual);
+    }
 
 
 
