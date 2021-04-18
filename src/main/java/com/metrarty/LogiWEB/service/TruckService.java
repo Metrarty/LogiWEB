13c2b92de1c5c341ed05069c21385883633871d4
Artem Oshkoder <metrarty@me.com>
2021-04-18 17:24:14 +0300

    t32: Moved Truck Selection to another class

--- src/main/java/com/metrarty/LogiWEB/service/TruckService.java
+++ src/main/java/com/metrarty/LogiWEB/service/TruckService.java
@@ -28,10 +28,8 @@ public class TruckService {
 
     private final TruckRepository truckRepository;
     private final TruckMapper truckMapper;
-    private final DistanceService distanceService;
-    private final CityService cityService;
-    private final CargoValidator cargoValidator;
     private final TruckValidator truckValidator;
+    private final TruckSelectionService truckSelectionService;
 
     /**
      * Creates truck and saves into repository.
@@ -62,20 +60,6 @@ public class TruckService {
         return result;
     }
 
-    /**
-     * Find all trucks with status FREE.
-     * @return List of trucks DTO
-     */
-    public List<TruckDto> findTrucksWithFreeStatus() {
-        log.info("TruckService.findTrucksWithFreeStatus was called");
-        List<Truck> entities = truckRepository.findAllByTruckStatus(TruckStatus.FREE.name());
-        List<TruckDto> result = new ArrayList<>();
-        for (Truck entity : entities) {
-            TruckDto truckDto = truckMapper.toDto(entity);
-            result.add(truckDto);
-        }
-        return result;
-    }
 
     /**
      * Edits truck with exact ID.
@@ -109,53 +93,8 @@ public class TruckService {
      * @param size order size
      * @return truck DTO
      */
-    public TruckDto chooseTruckToDeliver(@NonNull Long id, @NonNull Long size) {
-        cargoValidator.checkCargo(size);
-
-        CityDto cityOrder = cityService.findCityById(id);
-
-        List<TruckDto> allTrucksWithFreeStatus = findTrucksWithFreeStatus();
-        Map<TruckDto, CityDto> trucksSuitable = prepareSuitableTrucksMap(allTrucksWithFreeStatus, size);
-        List<TruckDto> result = checkIfTruckIsInCityOrder(trucksSuitable, cityOrder);
-
-        if (result.isEmpty()) {
-            List<DistanceDto> distanceSuitable = distanceService.prepareSuitableDistances(cityOrder);
-            distanceSuitable.sort(Comparator.comparingLong(DistanceDto::getDistance));
-            addToResultNearestTrucks(result, distanceSuitable, trucksSuitable);
-        }
-        return result.get(0);
-    }
-
-    private Map<TruckDto, CityDto> prepareSuitableTrucksMap(List<TruckDto> allTrucks, Long size) {
-        Map<TruckDto, CityDto> trucksSuitable = new HashMap<>();
-        for (TruckDto truck : allTrucks)
-            if (truck.getCapacity() >= size) {
-                trucksSuitable.put(truck, truck.getLocation());
-            }
-        return trucksSuitable;
-    }
-
-    private List<TruckDto> checkIfTruckIsInCityOrder(Map<TruckDto, CityDto> trucksSuitable, CityDto cityOrder) {
-        List<TruckDto> trucks = new ArrayList<>();
-        for (Map.Entry<TruckDto, CityDto> entry : trucksSuitable.entrySet()) {
-            if (entry.getValue().equals(cityOrder)) {
-                TruckDto key = entry.getKey();
-                trucks.add(key);
-            }
-        }
-        return trucks;
-    }
-
-    private void addToResultNearestTrucks(List<TruckDto> result, List<DistanceDto> distanceSuitable, Map<TruckDto, CityDto> trucksSuitable) {
-        for (DistanceDto distanceDto : distanceSuitable) {
-            for (Map.Entry<TruckDto, CityDto> entry : trucksSuitable.entrySet()) {
-                TruckDto key = entry.getKey();
-                CityDto value = entry.getValue();
-                if (value.equals(distanceDto.getCity1()) || value.equals(distanceDto.getCity2())) {
-                    result.add(key);
-                }
-            }
-        }
+    public TruckDto chooseTruckToDeliver(Long id, Long size) {
+        return truckSelectionService.chooseTruckToDeliver(id, size);
     }
 
     public Truck findOneTruckById(Long id) {
