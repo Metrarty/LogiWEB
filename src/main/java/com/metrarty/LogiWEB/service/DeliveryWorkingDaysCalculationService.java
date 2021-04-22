577187b516646636ea210f4435ce07ed8c539e05
Artem Oshkoder <metrarty@me.com>
2021-04-22 16:09:21 +0300

    t32: Fixed calculation method for cases like ((1+2)/3)=2

--- src/main/java/com/metrarty/LogiWEB/service/DeliveryWorkingDaysCalculationService.java
+++ src/main/java/com/metrarty/LogiWEB/service/DeliveryWorkingDaysCalculationService.java
@@ -7,6 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Component
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Log4j2
@@ -18,12 +21,13 @@ public class DeliveryWorkingDaysCalculationService {
         City orderDestination = order.getDestination();
         City orderSourceCity = order.getSourceCity();
         City truckLocation = order.getAssignedTruck().getLocation();
-        Long distancePerDay = order.getAssignedTruck().getDistancePerDay();
-        Long distanceBetweenDestinationAndSource = distanceService.distanceBetweenCities(orderDestination, orderSourceCity);
-        Long distanceFromTruckToSourceCity = distanceService.distanceBetweenCities(orderDestination, truckLocation);
-        return Math.toIntExact(((distanceFromTruckToSourceCity + distanceBetweenDestinationAndSource) / distancePerDay))+1;
+        Double distancePerDay = Double.valueOf(order.getAssignedTruck().getDistancePerDay());
+        Double distanceBetweenDestinationAndSource = Double.valueOf(distanceService.distanceBetweenCities(orderDestination, orderSourceCity));
+        Double distanceFromTruckToSourceCity = Double.valueOf(distanceService.distanceBetweenCities(orderDestination, truckLocation));
+        if (distanceFromTruckToSourceCity == 0) {
+            distanceFromTruckToSourceCity = distanceBetweenDestinationAndSource;
+        }
+        BigDecimal result = new BigDecimal((distanceFromTruckToSourceCity + distanceBetweenDestinationAndSource) / distancePerDay).setScale(0, RoundingMode.HALF_UP);
+        return result.intValue();
     }
-
-    // TODO metrarty 21.04.2021: it produces a wrong value if for example - > ( 6 / 3 ) + 1 = 4 instead of 3
-    //due to it this logic must be updated
 }
