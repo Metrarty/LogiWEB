93e3440295a9abc35ce47f81a13f3dcec3398727
Artem Oshkoder <metrarty@me.com>
2021-04-25 16:13:40 +0300

    t30: Bug fixes

--- src/main/java/com/metrarty/LogiWEB/service/validator/OrderValidator.java
+++ src/main/java/com/metrarty/LogiWEB/service/validator/OrderValidator.java
@@ -1,6 +1,5 @@
 package com.metrarty.LogiWEB.service.validator;
 
-import com.metrarty.LogiWEB.boundary.model.TruckDto;
 import com.metrarty.LogiWEB.repository.entity.Truck;
 import com.metrarty.LogiWEB.service.exception.EntityNotFoundException;
 import org.springframework.stereotype.Component;
@@ -9,12 +8,12 @@ import org.springframework.stereotype.Component;
 public class OrderValidator {
 
     public void checkOrderTruck(Truck assignedTruck) {
-        if (isTruckIdNull(assignedTruck)) {
+        if (isOrderTruckNull(assignedTruck)) {
             throw new EntityNotFoundException("Truck is not assigned to order.");
         }
     }
 
-    private boolean isTruckIdNull(Truck assignedTruck) {
+    private boolean isOrderTruckNull(Truck assignedTruck) {
         return assignedTruck == null;
     }
 }
