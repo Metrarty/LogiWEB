9d7d255dbed742c67068a9571c74eb582fa0a9c1
Artem Oshkoder <metrarty@me.com>
2021-05-10 15:41:10 +0300

    t43: Added tests for RestExceptionHandler

--- src/test/java/com/metrarty/LogiWEB/boundary/RestExceptionHandlerTest.java
+++ src/test/java/com/metrarty/LogiWEB/boundary/RestExceptionHandlerTest.java
@@ -1,13 +1,19 @@
 package com.metrarty.LogiWEB.boundary;
 
 import com.metrarty.LogiWEB.service.exception.EntityNotFoundException;
+import com.metrarty.LogiWEB.service.exception.ValueIsInvalidException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Collections;
+import java.util.List;
+
 
 public class RestExceptionHandlerTest {
 
@@ -28,7 +34,44 @@ public class RestExceptionHandlerTest {
         //test
         Assert.assertEquals("Must be equal", expected, actual);
     }
-}
 
-//todo: How to write test for RestExceptionHandler?
+    @Test
+    public void handleValueIsInvalidException() {
+        //prepare
+        ValueIsInvalidException ex = new ValueIsInvalidException("Test message");
+        ResponseEntity<String> expected = ResponseEntity.badRequest().body("Test message");
+        //run
+        ResponseEntity<String> actual = restExceptionHandler.handle(ex);
+        //test
+        Assert.assertEquals("Must be equal", expected, actual);
+    }
+
+    @Test
+    public void handleMethodArgumentNotValidException() {
+        //prepare;
+        ObjectError error = new ObjectError("testObject", "Test message");
+        BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(error, "testObject");
+        MethodArgumentNotValidException ex = new MethodArgumentNotValidException(null, bindingResult);
+        ex.addError(error);
+
+        List<String> result = Collections.singletonList("Test message");
+        ResponseEntity<List<String>> expected = ResponseEntity.badRequest().body(result);
+
+        //run
+        ResponseEntity<List<String>> actual = restExceptionHandler.handle(ex);
+        //test
+        Assert.assertEquals("Must be equal", expected, actual);
+    }
+
+    @Test
+    public void handleHttpMessageNotReadableException() {
+        //prepare
+        HttpMessageNotReadableException ex = new HttpMessageNotReadableException("HTTP body is not readable");
+        ResponseEntity<String> expected = ResponseEntity.badRequest().body("HTTP body is not readable");
+        //run
+        ResponseEntity<String> actual = restExceptionHandler.handle(ex);
+        //test
+        Assert.assertEquals("Must be equal", expected, actual);
+    }
+}
 
