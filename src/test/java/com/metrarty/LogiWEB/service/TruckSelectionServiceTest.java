b23c57c473d312e781e2b469d329e77b8a8eaae3
Artem Oshkoder <metrarty@me.com>
2021-05-18 09:29:10 +0300

    t43: Added tests for TruckSelectionService

--- src/test/java/com/metrarty/LogiWEB/service/TruckSelectionServiceTest.java
+++ src/test/java/com/metrarty/LogiWEB/service/TruckSelectionServiceTest.java
@@ -1,9 +1,11 @@
 package com.metrarty.LogiWEB.service;
 
 import com.metrarty.LogiWEB.boundary.model.CityDto;
+import com.metrarty.LogiWEB.boundary.model.DistanceDto;
 import com.metrarty.LogiWEB.boundary.model.TruckDto;
 import com.metrarty.LogiWEB.repository.TruckRepository;
 import com.metrarty.LogiWEB.repository.entity.City;
+import com.metrarty.LogiWEB.repository.entity.Distance;
 import com.metrarty.LogiWEB.repository.entity.Truck;
 import com.metrarty.LogiWEB.repository.entity.TruckStatus;
 import com.metrarty.LogiWEB.service.mapper.TruckMapper;
@@ -42,6 +44,45 @@ public class TruckSelectionServiceTest {
     private TruckMapper truckMapper;
 
     @Test
+    public void chooseTruckToDeliverTest() {
+        //prepare
+        CityDto cityOrderDto = new CityDto();
+        cityOrderDto.setId(1L);
+        Mockito.when(cityService.findCityById(1L)).thenReturn(cityOrderDto);
+
+        Truck truckWithFreeStatus = new Truck();
+        truckWithFreeStatus.setId(2L);
+        truckWithFreeStatus.setTruckStatus("FREE");
+        truckWithFreeStatus.setCapacity(5000L);
+        List<Truck> trucksWithFreeStatus = Collections.singletonList(truckWithFreeStatus);
+        Mockito.when(truckRepository.findAllByTruckStatus("FREE")).thenReturn(trucksWithFreeStatus);
+        TruckDto truckWithFreeStatusDto = new TruckDto();
+        truckWithFreeStatusDto.setId(2L);
+        truckWithFreeStatusDto.setTruckStatus(TruckStatus.valueOf("FREE"));
+        truckWithFreeStatusDto.setCapacity(5000L);
+        CityDto truckLocation = new CityDto();
+        truckLocation.setId(2L);
+        truckWithFreeStatusDto.setLocation(truckLocation);
+        Mockito.when(truckMapper.toDto(truckWithFreeStatus)).thenReturn(truckWithFreeStatusDto);
+
+        DistanceDto distance = new DistanceDto();
+        distance.setCity1(truckLocation);
+        distance.setCity2(cityOrderDto);
+        List<DistanceDto> distancesDto = Collections.singletonList(distance);
+        Mockito.when(distanceService.prepareSuitableDistances(cityOrderDto)).thenReturn(distancesDto);
+
+        //run
+        truckSelectionService.chooseTruckToDeliver(1L, 500L);
+        //test
+        Mockito.verify(cargoValidator, Mockito.times(1)).checkCargo(500L);
+        Mockito.verify(cityService, Mockito.times(1)).findCityById(1L);
+        Mockito.verify(truckRepository, Mockito.times(1)).findAllByTruckStatus("FREE");
+        Mockito.verify(truckMapper, Mockito.times(1)).toDto(truckWithFreeStatus);
+        Mockito.verify(distanceService, Mockito.times(1)).prepareSuitableDistances(cityOrderDto);
+        Mockito.verifyNoMoreInteractions(cargoValidator, cityService, truckRepository, truckMapper, distanceService);
+    }
+
+    @Test
     public void chooseTruckToDeliverTestWhenTruckInCityOrder() {
         //prepare
         CityDto cityOrderDto = new CityDto();
@@ -62,17 +103,14 @@ public class TruckSelectionServiceTest {
         truckLocation.setId(2L);
         truckWithFreeStatusDto.setLocation(cityOrderDto);
         Mockito.when(truckMapper.toDto(truckWithFreeStatus)).thenReturn(truckWithFreeStatusDto);
-        List<TruckDto> allTrucksWithFreeStatusDto = Collections.singletonList(truckWithFreeStatusDto);
-
-
-       // TruckDto truckSuitable = allTrucksWithFreeStatusDto.get(0);
-        //Map<TruckDto, CityDto> trucksSuitable = Collections.singletonMap(truckSuitable, truckLocation);
-
-       // TruckDto expected = result.get(0);
 
         //run
         truckSelectionService.chooseTruckToDeliver(1L, 500L);
         //test
-
+        Mockito.verify(cargoValidator, Mockito.times(1)).checkCargo(500L);
+        Mockito.verify(cityService, Mockito.times(1)).findCityById(1L);
+        Mockito.verify(truckRepository, Mockito.times(1)).findAllByTruckStatus("FREE");
+        Mockito.verify(truckMapper, Mockito.times(1)).toDto(truckWithFreeStatus);
+        Mockito.verifyNoMoreInteractions(cargoValidator, cityService, truckRepository, truckMapper);
     }
 }
